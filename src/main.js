afe6034ef825ee2472b81ae44cd0c079a6932282
眼镜 <1057427161@qq.com>
2020-01-19 18:47:06 +0800

    项目起步 - Element-ui整合

--- src/main.js
+++ src/main.js
@@ -1,11 +1,22 @@
+// vue 引入
 import Vue from 'vue'
+// app 引入
 import App from './App.vue'
+// router 引入
 import router from './router/index.js'
+// 全局css样式 引入
 import './style/base.css'
+// element 引入
+import ElementUI from 'element-ui'
+// element css样式
+import 'element-ui/lib/theme-chalk/index.css'
+// 注册element
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
 new Vue({
+  // 挂载注入 vue 实例
   router,
   render: h => h(App),
 }).$mount('#app')
